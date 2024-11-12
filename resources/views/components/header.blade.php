3ff61910ebc75eeb3cd55216fe5bfe75ecfe9abb
hpacheco9 <henrique.stuff15@gmail.com>
2024-11-12 14:39:42 -0100

    No drugs found

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -176,6 +176,13 @@
         width: 100%;
     }
 
+    input[type=search]::-ms-clear { display: none; width : 0; height: 0; }
+    input[type=search]::-ms-reveal { display: none; width : 0; height: 0; }
+    input[type="search"]::-webkit-search-decoration,
+    input[type="search"]::-webkit-search-cancel-button,
+    input[type="search"]::-webkit-search-results-button,
+    input[type="search"]::-webkit-search-results-decoration { display: none; }
+
 </style>
 
 <header>
