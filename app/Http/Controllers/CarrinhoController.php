e8568dbca4d62fad455ee2fe9020e59541e4987a
Henrique <henrique.stuff15@gmail.com>
2024-11-13 14:30:42 -0100

    carrinho func

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -15,14 +15,18 @@ class CarrinhoController extends Controller
         $userEmail = Auth::user()->email;
         $referencia = $request->referencia;
         $quantidade = $request->input('quantidade');
+
+
         Carrinho::firstOrCreate(['user_email' => $userEmail]);
-        return redirect()->route('carrinho.addOrCreate')->with([
+
+
+        return redirect()->route('carrinho.addOrCreate', [
             'user_email' => $userEmail,
             'referencia' => $referencia,
             'quantidade' => $quantidade
         ]);
-    }
 
+    }
 
 
 
