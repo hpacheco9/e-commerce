58577365c7e6ac213b3e617d17d04a2832177847
Miguel1i <miguelrego14@gmail.com>
2024-12-16 09:30:52 -0100

    fixed route on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -127,7 +127,7 @@
                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
                                         <div class="modal-body">
-                                            <form id="addMedicamentoForm" method="POST" action="/create">
+                                            <form id="addMedicamentoForm" method="POST" action="medicamentos/create">
                                                 @csrf
                                                 <div class="mb-3">
                                                     <label for="editReferencia" class="form-label">Referencia</label>
